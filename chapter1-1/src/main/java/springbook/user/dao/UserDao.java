c5ac774cfb8c59c3fb8ff1954e6dd85676749338
yangfriendship <yangfriendship@naver.com>
2022-03-25 21:17:08 +0900

    1.7.5 메소드를 이용한 의존관계 주입

--- chapter1-1/src/main/java/springbook/user/dao/UserDao.java
+++ chapter1-1/src/main/java/springbook/user/dao/UserDao.java
@@ -10,7 +10,14 @@ public class UserDao {
 
     private ConnectionMaker connectionMaker;
 
+    public UserDao() {
+    }
+
     public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
+    public void setConnectionMaker(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
 
