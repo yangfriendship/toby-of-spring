caa378a7f9d7394de13e4c80e7be06210c863278
yangfriendship <yangfriendship@naver.com>
2022-03-28 09:00:17 +0900

    5.1.5 코드 개선 - UpgradeLevel() 리팩토링

--- chapter5/src/main/java/springbook/user/service/UserService.java
+++ chapter5/src/main/java/springbook/user/service/UserService.java
@@ -18,19 +18,36 @@ public class UserService {
     public void upgradeLevels() {
         final List<User> users = userDao.getAll();
         for (final User user : users) {
-            boolean changed = true;
-            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-                user.setLevel(Level.SILVER);
-            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-                user.setLevel(Level.GOLD);
-            } else if (user.getLevel() == Level.GOLD) {
-                changed = false;
-            } else {
-                changed = false;
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+                this.userDao.update(user);
             }
-            if (changed) {
-                userDao.update(user);
+        }
+    }
+
+    private void upgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+        if (currentLevel == Level.BASIC) {
+            user.setLevel(Level.SILVER);
+        } else if (currentLevel == Level.SILVER) {
+            user.setLevel(Level.GOLD);
+        }
+    }
+
+    private boolean canUpgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+        switch (currentLevel) {
+            case BASIC: {
+                return user.getLogin() >= 50;
             }
+            case SILVER: {
+                return user.getRecommend() >= 30;
+            }
+            case GOLD: {
+                return false;
+            }
+            default:
+                throw new IllegalArgumentException("Unknown Level: " + currentLevel);
         }
     }
 
